fb443a44f08d43da18a38c1d2a4e8ff0eec23efb
connor <cjleahy95@googlemail.com>
2021-07-19 12:04:47 +0000

    cord

--- scripts/deploy_server.sh
+++ scripts/deploy_server.sh
@@ -3,9 +3,9 @@ set -e
 
 rm -r mesh-transformer-jax || true
 
-git clone https://github.com/kingoflolz/mesh-transformer-jax
+git clone https://github.com/ConnorJL/mesh-transformer-jax
 pip install -r mesh-transformer-jax/requirements.txt
 pip install mesh-transformer-jax/ jax==0.2.12
 
 pushd mesh-transformer-jax || exit
-screen -d -m python3 device_serve.py --config configs/6B_roto_256.json
+screen -d -m python3 device_serve.py --config configs/cord.json
